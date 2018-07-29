6b29f54bb94add62d03e0fd39d5265ec00287792
alexengrig <alexengrig@gmail.com>
2018-07-29 21:05:21 +0300

    Create domain objects

--- src/main/java/com/alexengrig/dirandfile/domain/SnapDirectory.java
+++ src/main/java/com/alexengrig/dirandfile/domain/SnapDirectory.java
@@ -1,4 +1,115 @@
 package com.alexengrig.dirandfile.domain;
 
+import java.util.Date;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Snapshot of directory
+ *
+ * @author G. Alex
+ */
 public class SnapDirectory {
+
+    /**
+     * Path to directory
+     */
+    private String path;
+    /**
+     * Date of snapshot
+     */
+    private Date date;
+
+    /**
+     * Number of directories
+     */
+    private int numDirs;
+    /**
+     * Number of files
+     */
+    private int numFiles;
+    /**
+     * Total size of nested files
+     */
+    private long totalSize;
+
+    private Set<SnapFile> files;
+
+    /* Constructors */
+
+    /**
+     * Create snapshot of directory, with path to directory and date of snapshot
+     *
+     * @param path Path to directory
+     * @param date Date of snapshot
+     */
+    public SnapDirectory(String path, Date date) {
+        this.path = path;
+        this.date = date;
+    }
+
+    /* Getters and Setters */
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getNumDirs() {
+        return numDirs;
+    }
+
+    public void setNumDirs(int numDirs) {
+        this.numDirs = numDirs;
+    }
+
+    public int getNumFiles() {
+        return numFiles;
+    }
+
+    public void setNumFiles(int numFiles) {
+        this.numFiles = numFiles;
+    }
+
+    public long getTotalSize() {
+        return totalSize;
+    }
+
+    public void setTotalSize(long totalSize) {
+        this.totalSize = totalSize;
+    }
+
+    public Set<SnapFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(Set<SnapFile> files) {
+        this.files = files;
+    }
+
+    /* Others */
+
+    @Override
+    public String toString() {
+        return "SnapDirectory{" +
+                "path='" + path + '\'' +
+                ", date=" + date +
+                ", numDirs=" + numDirs +
+                ", numFiles=" + numFiles +
+                ", totalSize=" + totalSize +
+                ", files=" + files.stream().map(Objects::toString).reduce(String::concat) +
+                '}';
+    }
+
 }
--- src/main/java/com/alexengrig/dirandfile/domain/SnapFile.java
+++ src/main/java/com/alexengrig/dirandfile/domain/SnapFile.java
@@ -1,4 +1,75 @@
 package com.alexengrig.dirandfile.domain;
 
+/**
+ * Snapshot of file
+ *
+ * @author G. Alex
+ */
 public class SnapFile {
+
+    /**
+     * File name
+     */
+    private String name;
+    /**
+     * File size
+     */
+    private long size;
+    /**
+     * File is a directory
+     */
+    private boolean isDirectory;
+
+    /* Constructors */
+
+    /**
+     * Create snapshot of file, with file name, file size and mark file is directory
+     *
+     * @param name        File name
+     * @param size        File size
+     * @param isDirectory Mark that file is directory
+     */
+    public SnapFile(String name, long size, boolean isDirectory) {
+        this.name = name;
+        this.size = size;
+        this.isDirectory = isDirectory;
+    }
+
+    /* Getters and Setters */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+
+    public void setDirectory(boolean directory) {
+        isDirectory = directory;
+    }
+
+    /* Others */
+
+    @Override
+    public String toString() {
+        return "SnapFile{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", isDirectory=" + isDirectory +
+                '}';
+    }
+
 }
