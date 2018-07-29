bfa15d91496d57fcb2f886188ae78a1a27712e51
alexengrig <alexengrig@gmail.com>
2018-07-29 21:26:34 +0300

    Create repository of snapdirectory

--- src/main/java/com/alexengrig/dirandfile/controller/DirsAndFilesController.java
+++ src/main/java/com/alexengrig/dirandfile/controller/DirsAndFilesController.java
@@ -1,15 +1,55 @@
 package com.alexengrig.dirandfile.controller;
 
+import com.alexengrig.dirandfile.FileSystemService;
+import com.alexengrig.dirandfile.domain.SnapDirectory;
+import com.alexengrig.dirandfile.repository.SnapDirectoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Controller of dirs and files
+ *
+ * @author G. Alex
+ */
 @Controller
 @RequestMapping("/dirs_and_files")
 public class DirsAndFilesController {
 
+    /**
+     * Repository of SnapDirectory
+     */
+    private SnapDirectoryRepository directoryRepository;
+
+    @Autowired
+    public DirsAndFilesController(SnapDirectoryRepository directoryRepository) {
+        this.directoryRepository = directoryRepository;
+    }
+
     @GetMapping
-    public String main() {
+    public String main(Model model) {
+
+        addAllDirectories(model);
         return "dirsandfiles";
+    }
+
+    @PostMapping
+    public String addDirByPath(@RequestParam String path
+            , Model model) {
+
+        SnapDirectory directory = FileSystemService.createSnapDirectory(path);
+        directoryRepository.save(directory);
+
+        addAllDirectories(model);
+        return "dirsandfiles";
+    }
+
+    private void addAllDirectories(Model model) {
+        Iterable<SnapDirectory> dirs = directoryRepository.findAll();
+        model.addAttribute("dirs", dirs);
     }
 }
---
+++ src/main/java/com/alexengrig/dirandfile/repository/SnapDirectoryRepository.java
@@ -0,0 +1,12 @@
+package com.alexengrig.dirandfile.repository;
+
+import com.alexengrig.dirandfile.domain.SnapDirectory;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Repository of SnapDirectory
+ *
+ * @author G. Alex
+ */
+public interface SnapDirectoryRepository extends CrudRepository<SnapDirectory, Long> {
+}
