e1eb8b672c60b9814f7919341193cf7b9ef29867
alexengrig <alexengrig@gmail.com>
2018-07-29 21:10:38 +0300

    Create greeting controller

---
+++ src/main/java/com/alexengrig/dirandfile/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.alexengrig.dirandfile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/", "/greeting", "/home"})
+public class GreetingController {
+
+    @GetMapping
+    public String main() {
+        return "greeting";
+    }
+
+}
---
+++ src/main/resources/templates/greeting.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Greeting</title>
+</head>
+<body>
+<h1>Hello</h1>
+<p>Go to page <a href="/dirs_and_files">dirs and files</a></p>
+</body>
+</html>
